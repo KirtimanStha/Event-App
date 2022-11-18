85dcf9f603936fb048628de7be36cedf6d61b410
Kirti Man Shrestha <kirtiman@ktmrush.com>
2022-11-18 17:16:46 +0545

    Add Back Buttons to Pages

--- resources/js/components/Event.vue
+++ resources/js/components/Event.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <div class="card-header text-end">
+            <router-link :to='{name:"home"}'><button class="btn btn-sm btn-info">Back</button></router-link>
+        </div>
         <div class="card">
             <div class="card-body">
                 <h5>{{ event.title }}</h5>
